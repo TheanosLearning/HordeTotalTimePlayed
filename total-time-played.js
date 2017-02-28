09c412be1adc7d625bf5ef6adcca9abacc3f09d7
carlcorder <corder.carl@gmail.com>
2017-02-27 20:01:41 -0600

    updates for tracking credits and modularizing functions

--- total-time-played.js
+++ total-time-played.js
@@ -1,43 +1,56 @@
-function hordeTimePlayed() {
+(function() {
+    var gow4Data = JSON.parse(document.getElementById('initialState').textContent);
 
-    const hordeClasses = ['Soldier', 'Sniper', 'Scout', 'Heavy', 'Engineer'];
-    const unitTime = ['d', 'h', 'm'];
-    const hordeDataId = 'initialState';
-    var hordeData = JSON.parse(document.getElementById(hordeDataId).textContent).horde;
-    var timePlayed = [];
-    var totalTimePlayed = [0,0,0];
+    function hordeTimePlayed(gow4Data) {
 
-    for(let i = 0; i < hordeClasses.length; i++) {
-        timePlayed.push(
-            hordeData.ModeStats[i].Stats[0].Value
-                .replace(/d|h|m/gi, '').split(' ').map(Number)
-        );
+        const hordeClasses = ['Soldier', 'Sniper', 'Scout', 'Heavy', 'Engineer'];
+        const unitTime = ['d', 'h', 'm'];
+        let hordeData = gow4Data.horde;
+        let timePlayed = [];
+        let totalTimePlayed = [0, 0, 0];
+
+        for (let i = 0; i < hordeClasses.length; i++) {
+            timePlayed.push(
+                hordeData.ModeStats[i].Stats[0].Value
+                    .replace(/d|h|m/gi, '').split(' ').map(Number)
+            );
+        }
+
+        for (let i = 0; i < unitTime.length; i++) {
+            for (let j = 0; j < hordeClasses.length; j++) {
+                totalTimePlayed[i] += timePlayed[j][i];
+            }
+        }
+
+        let total = reduceTotalTime(totalTimePlayed);
+        displayTotalTime(total);
     }
 
-    // sum d/h/m over all classes
-    for(let i = 0; i < unitTime.length; i++) {
-        for(let j = 0; j < hordeClasses.length; j++) {
-            totalTimePlayed[i] += timePlayed[j][i];
+    function reduceTotalTime(totalTime) {
+        const minutes = totalTime[2] % 60;
+        const hoursFromMinutes = Math.floor(totalTime[2] / 60);
+        const hours = (totalTime[1] + hoursFromMinutes) % 24;
+        const daysFromHours = Math.floor((totalTime[1] + hoursFromMinutes) / 24);
+        const days = totalTime[0] + daysFromHours;
+
+        return [days, hours, minutes];
+    }
+
+    function displayTotalTime(totalReduced, active) {
+        let hordeTitleDiv = Array.from(document.querySelectorAll('.module-title'))
+            .filter(div => div.innerText
+                .includes("HORDE"))[0];
+
+        if (document.getElementById('horde-total-time') == null) {
+            let timePlayedNode = document.createElement('h4');
+            timePlayedNode.id = "horde-total-time";
+            timePlayedNode.align = "center";
+            timePlayedNode.innerHTML = "TOTAL TIME PLAYED: " +
+                totalReduced[0] + "D " + totalReduced[1] + "H " + totalReduced[2] + "M";
+
+            hordeTitleDiv.parentNode.insertBefore(timePlayedNode, hordeTitleDiv.nextSibling);
         }
     }
 
-    // put total time in lowest terms i.e. 95m -> 1h + 35m
-    const minutes = totalTimePlayed[2] % 60;
-    const hoursFromMinutes = Math.floor(totalTimePlayed[2] / 60);
-    const hours = (totalTimePlayed[1] + hoursFromMinutes) % 24;
-    const daysFromHours = Math.floor((totalTimePlayed[1] + hoursFromMinutes) / 24);
-    const days = totalTimePlayed[0] + daysFromHours;
-
-    const totalReduced = [days, hours, minutes];
-    //const workWeeks = (24 * days + hours + (minutes / 60)) / 40;
-
-    let hordeTitleText = "HORDE";
-    let hordeTitleDiv = Array.from(document.querySelectorAll('.module-title')).filter(div => div.innerText.includes(hordeTitleText))[0];
-
-    // build display node
-    let timePlayedNode = document.createElement('h4');
-    timePlayedNode.innerHTML = "TOTAL TIME PLAYED: " + totalReduced[0] + "D " + totalReduced[1] + "H " + totalReduced[2] + "M";
-    timePlayedNode.align="center";
-
-    hordeTitleDiv.parentNode.insertBefore(timePlayedNode, hordeTitleDiv.nextSibling);
-}
+    hordeTimePlayed(gow4Data);
+})();
